4880c755971359f8f7a9a9c5b5d2ad8068b47228
Masafumi Horimoto on comet01 <horimoto@holly-linux.com>
2019-06-26 01:32:01 +0900

    V1.20 ntpdの停止を監視して、cndで報告

--- todtalkerd.py
+++ todtalkerd.py
@@ -2,7 +2,7 @@
 #coding: utf-8
 #
 # TOD Talker
-# Version 1.10
+# Version 1.20
 # Date 2019/06/25
 # Author M.Horimoto
 #
@@ -12,6 +12,7 @@ import time
 import configparser
 import netifaces
 from socket import *
+from subprocess import check_output,Popen
 
 XML_HEADER  = "<?xml version=\"1.0\"?>"
 UECS_HEADER = "<UECS ver=\"1.00-E10\">"
@@ -36,7 +37,8 @@ config.read('/etc/uecs/config.ini')
 
 lcd.lcd_init()
 prevsec = 0
-ip = HOST
+itv     = 0
+ip      = HOST
 
 while(True):
     a=datetime.datetime.now()
@@ -48,9 +50,11 @@ while(True):
         u = "U:{0}".format(s)
         lcd.lcd_string(u,l)
         tn = "Time"
-        send_UECSdata(tn,config[tn]['room'],config[tn]['region'],config[tn]['order'],config[tn]['priority'],t,HOST)
+        send_UECSdata(tn,config[tn]['room'],config[tn]['region'],\
+                      config[tn]['order'],config[tn]['priority'],t,HOST)
         tn = "Date"
-        send_UECSdata(tn,config[tn]['room'],config[tn]['region'],config[tn]['order'],config[tn]['priority'],d,HOST)
+        send_UECSdata(tn,config[tn]['room'],config[tn]['region'],\
+                      config[tn]['order'],config[tn]['priority'],d,HOST)
     l = lcd.LCD_LINE_1
     lcd.lcd_string(s,l)
     if (a.second>50):
@@ -66,7 +70,19 @@ while(True):
     elif (a.second>10):
         lcd.lcd_string(ip,lcd.LCD_LINE_2)
     prevsec = a.second
-    time.sleep(1)
-    tn = "cnd.mXX"
-    cndv = 0
-    send_UECSdata(tn,config[tn]['room'],config[tn]['region'],config[tn]['order'],config[tn]['priority'],cndv,HOST)
+    time.sleep(0.01)
+    itv += 1
+    if (itv>=8):
+        tn = "cnd.mXX"
+        pcmd = "ps ax | grep /usr/sbin/ntpd | grep -v grep | wc -l"
+        ouv  = check_output(pcmd,shell=True)
+        if (int(ouv)==0):
+            cndv = 1048576 # No ntp daemon running
+            pcmd = "systemctl restart ntp"
+            popn = Popen(pcmd,shell=True)
+        else:
+            cndv = 0
+        send_UECSdata(tn,config[tn]['room'],config[tn]['region'],\
+                      config[tn]['order'],config[tn]['priority'],cndv,HOST)
+        itv = 0
+
